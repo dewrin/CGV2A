f5ed45c29e46d107284bb9edd5d8424817acf1b0
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-29 21:33:10 +0100

    feat: add login.jsx and login.css

---
+++ src/pages/Login.jsx
@@ -0,0 +1,30 @@
+import '../styles/Login.css';
+
+import { handleLogin } from '../utils/function';
+
+const Login = ({ theme, setAuthentication }) => {
+  return (
+    <section className={`Login Login-${theme}`}>
+      <form className={`login-form login-form-${theme}`} onSubmit={(e) => handleLogin(e, setAuthentication)}>
+        <h1 className={`login-form-logo`}>Insert title here..</h1>
+        <input
+          className={`login-form-input login-form-input-${theme}`}
+          id="username"
+          type="text"
+          placeholder="username"
+        />
+        <input
+          className={`login-form-input login-form-input-${theme}`}
+          id="password"
+          type="password"
+          placeholder="password"
+        />
+        <button className={`login-form-submit login-form-submit-${theme}`} type="submit">
+          LOGIN
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
