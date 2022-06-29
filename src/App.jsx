5f6e246b05bf7d83fa18e329b8afcf96952d74c4
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-29 22:02:07 +0100

    feat: render toast container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles/App.css';
 
 import Login from './pages/Login';
@@ -14,28 +16,34 @@ function App() {
 
   if (isAuthenticated) {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>HOMEPAGE</h1>} />
-          <Route path="/login" element={<Navigate to="/profile" />} />
-          <Route path="/create" element={<Navigate to="/profile" />} />
-          <Route path="/profile" element={<h1>PROFILE</h1>} />
-          <Route path="/*" element={<h1>404 PAGE NOT FOUND</h1>} />
-        </Routes>
-      </BrowserRouter>
+      <>
+        <ToastContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>HOMEPAGE</h1>} />
+            <Route path="/login" element={<Navigate to="/profile" />} />
+            <Route path="/create" element={<Navigate to="/profile" />} />
+            <Route path="/profile" element={<h1>PROFILE</h1>} />
+            <Route path="/*" element={<h1>404 PAGE NOT FOUND</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </>
     );
   }
 
   if (!isAuthenticated) {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>HOMEPAGE</h1>} />
-          <Route path="/login" element={<Login theme={theme} setAuthentication={setAuthentication} />} />
-          <Route path="/create" element={<h1>CREATE</h1>} />
-          <Route path="/*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
+      <>
+        <ToastContainer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>HOMEPAGE</h1>} />
+            <Route path="/login" element={<Login theme={theme} setAuthentication={setAuthentication} />} />
+            <Route path="/create" element={<h1>CREATE</h1>} />
+            <Route path="/*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     );
   }
 }
