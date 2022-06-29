d9ed20bba2efcec04d1b13d06aecea649753197c
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-29 15:20:52 +0100

    feat: setup react-router-dom renders and redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
 import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>HEY</h1>
-    </div>
-  );
+  const [authentication, setAuthentication] = useState({
+    isAuthenticated: true,
+  });
+
+  const { isAuthenticated } = authentication;
+
+  if (isAuthenticated) {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>HOMEPAGE</h1>} />
+          <Route path="/login" element={<Navigate to="/profile" />} />
+          <Route path="/create" element={<Navigate to="/profile" />} />
+          <Route path="/profile" element={<h1>PROFILE</h1>} />
+          <Route path="/*" element={<h1>404 PAGE NOT FOUND</h1>} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>HOMEPAGE</h1>} />
+          <Route path="/login" element={<h1>LOGIN</h1>} />
+          <Route path="/create" element={<h1>CREATE</h1>} />
+          <Route path="/*" element={<Navigate to="/login" />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
