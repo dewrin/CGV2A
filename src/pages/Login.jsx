86a4f850fb76a6d30b59d250f8a1bb6de767f221
Gareth Dawson <gwdawson.work@gmail.com>
2022-07-01 11:01:27 +0100

    feat: add theme switcher to login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,18 @@
 import '../styles/Login.css';
+import dark from '../assets/dark.png';
+import light from '../assets/light.png';
 
 import { handleLogin } from '../utils/function';
 
 const Login = ({ theme, setAuthentication }) => {
   return (
     <section className={`Login Login-${theme}`}>
+      <img
+        className={`login-theme-switch`}
+        src={theme === 'light' ? light : dark}
+        alt="theme"
+        onClick={() => setAuthentication((prev) => ({ ...prev, theme: theme === 'light' ? 'dark' : 'light' }))}
+      />
       <form className={`login-form login-form-${theme}`} onSubmit={(e) => handleLogin(e, setAuthentication, theme)}>
         <h1 className={`login-form-logo`}>Insert title here..</h1>
         <input
