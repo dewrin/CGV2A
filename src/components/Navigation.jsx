176937e662d6aae20f992007b606e714e1dd2b2d
Gareth Dawson <gwdawson.work@gmail.com>
2022-07-03 21:54:35 +0100

    feat: add navigation link icons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,3 +1,5 @@
+import { MdOutlineLocalGroceryStore } from 'react-icons/md';
+import { CgProfile, CgCardHearts } from 'react-icons/cg';
 import { Link } from 'react-router-dom';
 import '../styles/Navigation.css';
 
@@ -8,12 +10,15 @@ const Navigation = ({ theme }) => {
       <div className="flex" style={{ display: 'flex', flexDirection: 'column' }}>
         <input className={`navigation-input navigation-input-${theme}`} type="text" placeholder="Search..." />
         <Link className={`navigation-link navigation-link-${theme}`} to="/profile">
+          <CgProfile className={'navigation-link-icon'} />
           Profile
         </Link>
         <Link className={`navigation-link navigation-link-${theme}`} to="/collection">
+          <CgCardHearts className={'navigation-link-icon'} />
           Collection
         </Link>
         <Link className={`navigation-link navigation-link-${theme}`} to="/store">
+          <MdOutlineLocalGroceryStore className={'navigation-link-icon'} />
           Store
         </Link>
       </div>
