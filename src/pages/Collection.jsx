09a6f4e605c6e9e3efb3eb24facd096a73e53f80
Gareth Dawson <gwdawson.work@gmail.com>
2022-07-05 17:23:35 +0100

    feat: render usercards on collection.jsx

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,10 +1,23 @@
 import '../styles/Collection.css';
 
-const Collection = ({ theme }) => {
+const Collection = ({ theme, authentication }) => {
+  const { usercards } = authentication;
   return (
     <section className={`Collection Collection-${theme}`}>
       <h1 className={`collection-title`}>COLLECTION</h1>
       <hr className={`collection-hr-${theme}`} />
+      <div className={`collection-cards`}>
+        {usercards.map((usercard) => {
+          return (
+            <img
+              className={`collection-card`}
+              src={usercard.imageurl}
+              key={Math.random()}
+              alt={`${usercard.group} ${usercard.idol}`}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 };
