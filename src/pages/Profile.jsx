947651d4330b3582363cb6d53c2aa468fdcf1d49
Gareth Dawson <gwdawson.work@gmail.com>
2022-07-05 14:19:42 +0100

    feat: render userinfo on profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,22 @@
 import '../styles/Profile.css';
 
-const Profile = ({ theme }) => {
+const Profile = ({ theme, authentication }) => {
+  // [authentication.joindate, authentication.currency, authentication.cards, authentication.trades, authentication.packs]
+
   return (
     <section className={`Profile Profile-${theme}`}>
       <h1 className={`profile-title`}>PROFILE</h1>
       <hr className={`profile-hr-${theme}`} />
+      <div className={`profile-info`}>
+        <img className={`profile-info-avatar`} src={authentication.avatar} alt="Avatar" />
+        <div className={`profile-info-text`}>
+          <h2 className={`profile-info-username`}>
+            {authentication.username.slice(0, 1).toUpperCase()}
+            {authentication.username.slice(1)}
+          </h2>
+          <h3 className={`profile-info-userid`}>{authentication.userid}</h3>
+        </div>
+      </div>
     </section>
   );
 };
